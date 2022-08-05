97961595fdefd01df8de010f938c86f9bb53efac
Teddy Horvath <teddyhorvath@gmail.com>
2022-08-04 17:06:09 -0700

    Implemented call back handlers

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     {
       title: 'React',
       url: 'https://reactjs.org/',
-      author: 'Jordan Aids',
+      author: 'Jordan Cancer',
       num_comments: 3,
       points: 4,
       objectID: 0
@@ -21,27 +21,46 @@ const App = () => {
       objectID: 1
     }
   ]
+
+    // A
+    const handleSearch = (event) => 
+    {
+      // C
+      console.log(event.target.value)
+    }
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search/>
+
+      {/* B */}
+      <Search onSearch={handleSearch}/>
       <hr/>
       <List list={stories}/>
     </div>
   );
+ 
 }
 
-const Search = () =>
+const Search = (props) =>
 {
-  const handleChange = (event) => 
-  {
-    console.log(event.target.value)
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value)
+
+    // B
+    props.onSearch(event)
   }
 
   return (
     <div>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' onChange={handleChange}/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
   )
 }
@@ -50,24 +69,25 @@ const List = (props) =>
 {
   return (
     <ul>
-      {props.list.map((item) =>
-      {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        )
-      })}
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
     </ul>
   )
 }
 
-export default App
+const Item = (props) => (
+ <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+)
+
+ export default App
 
 
 
