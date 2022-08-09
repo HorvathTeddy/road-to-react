6071103ba551f4c0c27cda3df0625706d43b00ee
Teddy Horvath <teddyhorvath@gmail.com>
2022-08-09 14:39:56 -0700

    Implement more concise React fragment

--- src/App.js
+++ src/App.js
@@ -60,17 +60,18 @@ const App = () => {
   );
 }
 
-const Search = ({ search, onSearch }) => 
-[
-<label key="1" htmlFor='search'>Search:{' '}</label>,
+const Search = ({ search, onSearch }) => (
+<>
+<label key="1" htmlFor='search'>Search:{' '}</label>
 <input 
   key="2"
   id='search' 
   type='text' 
   value={search} 
   onChange={onSearch} 
-/>,
-]
+/>
+</>
+)
 
 
 const List = ({ list }) => 
